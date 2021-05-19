455fa742b56f71ec183f8a8e5b30a9c036511519
JvoProgramming <jvoprogramming@gmail.com>
2021-05-19 12:42:34 -0700

    added main() functionality

--- main.cpp
+++ main.cpp
@@ -10,31 +10,28 @@ using namespace std;
 
 int main(){
     srand(time(NULL));
-    /*int input, choice;
+    int numFeatures, choice;
     
     cout << "Welcome to Johnny and Diego's Feature Selection Algorithm." << endl;
     cout << "Please enter total number of features: ";
-    cin >> input;
-    cout << "/n Type the number of the algorithm you want to run." << endl;
-    cout << "/t 1) Forward Selection" << endl;
-    cout << "/t 2) Backward Elimination" << endl;
-    cout << "/t 3) Johnny and Diego's Special Algorithm" << endl;
-
-    if(choice == 1){
-        forward();
-    }
-    else if(choice == 2){
-        backward();
-    }
-    else{
-
-    }
-
-    */
+    cin >> numFeatures;
+    cout << "Type the number of the algorithm you want to run." << endl;
+    cout << "1) Forward Selection" << endl;
+    cout << "2) Backward Elimination" << endl;
+    cout << "3) Johnny and Diego's Special Algorithm" << endl;
+    cin >> choice;
 
     featureNode* test = new featureNode();
-    greedySearch* greedyObj = new greedySearch(4);
-    greedyObj->search(test);
+    greedySearch* greedyObj = new greedySearch(numFeatures);
+    
+    switch(choice)
+    {
+        case 1:
+            greedyObj->search(test);
+            break;
+        case 2:
+            break;
+    }
 
 
    return 0;
